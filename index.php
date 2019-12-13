d38b4a61ad03fa0032f14fabe8c9ddab4f95944c
LudoGit19 <54305244+LudoGit19@users.noreply.github.com>
2019-12-13 11:35:26 +0100

    Clean

--- index.php
+++ index.php
@@ -4,15 +4,3 @@ include("tools/menu.php");
 include("views/loginView.php");
 include("views/registerView.php");
 ?>
-
-<!-- 
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-  
-    <title>Mon cv en ligne</title>
-</head>
-<body>
-    
-</body>
-</html> -->
